513eb9f9dc3227c3a70002dca57653d95d533f6b
edgar <strazh.goroda@gmail.com>
2017-03-16 20:23:45 +0200

    #1 rename

---
+++ src/main/java/com/spd/dto/UserRegistrationDTO.java
@@ -0,0 +1,16 @@
+package com.spd.dto;
+
+
+public class UserRegistrationDTO {
+
+    public String firstName;
+
+    public String lastName;
+
+    public String email;
+
+    public String password;
+
+    public String termsChecked;
+
+}
