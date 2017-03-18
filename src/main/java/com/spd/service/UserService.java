0bfa0fd04ef4d02babd109a46ccb174f1e523c65
edgar <strazh.goroda@gmail.com>
2017-03-18 21:07:24 +0200

    #2 service

--- src/main/java/com/spd/service/UserService.java
+++ src/main/java/com/spd/service/UserService.java
@@ -1,9 +1,13 @@
 package com.spd.service;
 
-import com.spd.dto.UserRegistrationDTO;
 import com.spd.entity.User;
+import com.spd.entity.UserEmail;
+import com.spd.entity.UserTelephone;
+import com.spd.repository.UserEmailRepository;
 import com.spd.repository.UserRepository;
+import com.spd.repository.UserTelephoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,16 +18,57 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserEmailRepository userEmailRepository;
+
+    @Autowired
+    private UserTelephoneRepository userTelephoneRepository;
+
     public User getById(int id) {
         return userRepository.getOne(id);
     }
 
-    public void saveUser(UserRegistrationDTO userRegistrationDTO) {
-        User user = new User();
-        user.setFirstName(userRegistrationDTO.getFirstName());
-        user.setLastName(userRegistrationDTO.getLastName());
-        user.setEmail(userRegistrationDTO.getEmail());
+    public void saveUser(User user) {
         userRepository.save(user);
+    }
+
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    public boolean deleteUser(int id) {
+        try {
+            userRepository.delete(id);
+        } catch (EmptyResultDataAccessException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void addExtraEmailByUser(int userId, String email) {
+        User user = userRepository.findOne(userId);
+        UserEmail userEmail = new UserEmail();
+        userEmail.setEmail(email);
+        userEmail.setUser(user);
+        userEmailRepository.save(userEmail);
+    }
+
+    public void deleteExtraEmailByUser(int userId, String email) {
+        UserEmail userEmail = userEmailRepository.findByUserIdAndEmail(userId, email);
+        userEmailRepository.delete(userEmail.getId());
+    }
+
+    public void addExtraTelephoneByUser(int userId, String telephone) {
+        User user = userRepository.findOne(userId);
+        UserTelephone userTelephone = new UserTelephone();
+        userTelephone.setTelephone(telephone);
+        userTelephone.setUser(user);
+        userTelephoneRepository.save(userTelephone);
+    }
+
+    public void deleteExtraTelephoneByUser(int userId, String telephone) {
+        UserTelephone userTelephone = userTelephoneRepository.findByUserIdAndTelephone(userId, telephone);
+        userTelephoneRepository.delete(userTelephone.getId());
     }
 
     public User findByEmail(String email) {
