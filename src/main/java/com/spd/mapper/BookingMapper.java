b9eb6322f48ed7dd26167a6f524909d34e844558
edgar <strazh.goroda@gmail.com>
2017-03-18 21:09:16 +0200

    #2 Added mapper

---
+++ src/main/java/com/spd/mapper/BookingMapper.java
@@ -0,0 +1,21 @@
+package com.spd.mapper;
+
+import com.spd.dto.UserInformationDTO;
+import com.spd.entity.User;
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.ConfigurableMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookingMapper extends ConfigurableMapper {
+
+    @Override
+    protected void configure(MapperFactory mapperFactory) {
+        mapperFactory.classMap(User.class, UserInformationDTO.class)
+                .field("userEmails{email}", "extraEmails{}")
+                .field("userTelephones{telephone}", "extraTelephones{}")
+                .byDefault()
+                .register();
+    }
+
+}
