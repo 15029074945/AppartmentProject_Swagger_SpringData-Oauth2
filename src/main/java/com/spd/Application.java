1068e3ac337064aa4bede4f87f36596b440deab1
edgar <strazh.goroda@gmail.com>
2017-03-18 21:11:11 +0200

    #3 application

--- src/main/java/com/spd/Application.java
+++ src/main/java/com/spd/Application.java
@@ -1,39 +1,16 @@
 package com.spd;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableJpaRepositories
 @SpringBootApplication
-@EnableTransactionManagement
-@EnableAutoConfiguration//(exclude = {SecurityAutoConfiguration.class})
-@EnableWebSecurity
-@EnableSwagger2
-@EnableResourceServer
-@EnableAuthorizationServer
+//@EnableTransactionManagement
+//@EnableAutoConfiguration
+//@EnableSwagger2
 public class Application {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-		};
-	}
 }
