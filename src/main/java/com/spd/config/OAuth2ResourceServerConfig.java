1a63716828c867d6e1a863a1da5187ff15bd8270
edgar <strazh.goroda@gmail.com>
2017-03-21 20:55:50 +0200

    #4 Registration permit all

--- src/main/java/com/spd/config/OAuth2ResourceServerConfig.java
+++ src/main/java/com/spd/config/OAuth2ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.spd.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -21,6 +22,8 @@ public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
                         "/webjars/**",
                         "/images/*.*")
                             .permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/users")
+                    .permitAll()
                 .anyRequest().authenticated();
     }
 
