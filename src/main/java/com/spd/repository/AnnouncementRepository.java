849857d54af8b8f148c3230b79a883b63e95c87d
edgar <strazh.goroda@gmail.com>
2017-03-28 00:43:32 +0300

    Added methods to announcement repository

--- src/main/java/com/spd/repository/AnnouncementRepository.java
+++ src/main/java/com/spd/repository/AnnouncementRepository.java
@@ -1,13 +1,18 @@
 package com.spd.repository;
 
+import com.spd.bean.AnnouncementBean;
 import com.spd.entity.Announcement;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
 
-    Optional<Announcement> findOneByUserId(Integer id);
+    Optional<Announcement> findOneById(Integer id);
+
+    List<AnnouncementBean> findByUserId(Integer id);
+
 }
