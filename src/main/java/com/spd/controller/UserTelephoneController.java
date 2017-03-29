097274ab27d66339f316793fe43f37e5e9f50750
edgar <strazh.goroda@gmail.com>
2017-03-29 18:02:58 +0300

    Fix telephone controller

--- src/main/java/com/spd/controller/UserTelephoneController.java
+++ src/main/java/com/spd/controller/UserTelephoneController.java
@@ -6,10 +6,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
