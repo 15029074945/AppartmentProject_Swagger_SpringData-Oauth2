1dd22970a94fd5d26ac4deb89a14a5a57b308e13
edgar <strazh.goroda@gmail.com>
2017-03-29 16:44:27 +0300

    Fix PUT in cors filter

--- src/main/java/com/spd/config/SimpleCORSFilter.java
+++ src/main/java/com/spd/config/SimpleCORSFilter.java
@@ -27,7 +27,7 @@ public class SimpleCORSFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "access-control-allow-credentials,access-control-allow-origin,authorization" +
                 ",Content-Type,x-requested-with,origin,accept,client-security-token");
