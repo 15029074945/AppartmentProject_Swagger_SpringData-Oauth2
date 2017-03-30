0756046ce36e1ccde1bebaeb42e085a730e21e17
edgar <strazh.goroda@gmail.com>
2017-03-30 20:18:48 +0300

    Added exceptions to global handler

--- src/main/java/com/spd/config/GlobalExceptionHandler.java
+++ src/main/java/com/spd/config/GlobalExceptionHandler.java
@@ -1,79 +1,70 @@
 package com.spd.config;
 
 import com.spd.bean.ErrorModelBean;
-import org.hibernate.service.spi.ServiceException;
-import org.springframework.http.HttpHeaders;
+import com.spd.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<ErrorModelBean> handleError(Exception ex) {
+    private static final String DEFAULT_MESSAGE = "(Global) ";
+    private static final HttpStatus DEFAULT_HTTP_STATUS = HttpStatus.INTERNAL_SERVER_ERROR;
 
-        ErrorModelBean errorModelBean = new ErrorModelBean("111" + ex.getMessage(),
-                HttpStatus.INTERNAL_SERVER_ERROR.value());
-
-        return new ResponseEntity<>(errorModelBean, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(value = AuthenticationUserException.class)
+    public ResponseEntity<ErrorModelBean> authenticationUserException(Exception ex) {
+        return new ResponseEntity<>(new ErrorModelBean(DEFAULT_MESSAGE + ex.getMessage()), DEFAULT_HTTP_STATUS);
     }
+
+    @ExceptionHandler(value = CheckFailedException.class)
+    public ResponseEntity<ErrorModelBean> checkFailedException(Exception ex) {
+        return new ResponseEntity<>(new ErrorModelBean(DEFAULT_MESSAGE + ex.getMessage()), DEFAULT_HTTP_STATUS);
+    }
+
+    @ExceptionHandler(value = FilledException.class)
+    public ResponseEntity<ErrorModelBean> filledException(Exception ex) {
+        return new ResponseEntity<>(new ErrorModelBean(DEFAULT_MESSAGE + ex.getMessage()), DEFAULT_HTTP_STATUS);
+    }
+
+    @ExceptionHandler(value = NoSuchAddressException.class)
+    public ResponseEntity<ErrorModelBean> noSuchAddressException(Exception ex) {
+        return new ResponseEntity<>(new ErrorModelBean(DEFAULT_MESSAGE + ex.getMessage()), DEFAULT_HTTP_STATUS);
+    }
+
+    @ExceptionHandler(value = NoSuchAnnouncementException.class)
+    public ResponseEntity<ErrorModelBean> noSuchAnnouncementException(Exception ex) {
+        return new ResponseEntity<>(new ErrorModelBean(DEFAULT_MESSAGE + ex.getMessage()), DEFAULT_HTTP_STATUS);
+    }
+
+    @ExceptionHandler(value = NoSuchUserException.class)
+    public ResponseEntity<ErrorModelBean> noSuchUserException(Exception ex) {
+        return new ResponseEntity<>(new ErrorModelBean(DEFAULT_MESSAGE + ex.getMessage()), DEFAULT_HTTP_STATUS);
+    }
+
+    @ExceptionHandler(value = ValidationException.class)
+    public ResponseEntity<ErrorModelBean> validationException(Exception ex) {
+        return new ResponseEntity<>(new ErrorModelBean(DEFAULT_MESSAGE + ex.getMessage()), DEFAULT_HTTP_STATUS);
+    }
+
 
     @ExceptionHandler(value = LoginAuthenticationProvider.AuthenticationExceptionImpl.class)
     public ResponseEntity<ErrorModelBean> handleAuthentication(Exception ex, WebRequest request) {
-        ErrorModelBean errorModelBean = new ErrorModelBean("111" + ex.getMessage(),
-                HttpStatus.UNAUTHORIZED.value());
+        ErrorModelBean errorModelBean = new ErrorModelBean(DEFAULT_MESSAGE + ex.getMessage());
 
         return new ResponseEntity<>(errorModelBean, HttpStatus.UNAUTHORIZED);
     }
 
-    /*@ExceptionHandler(Exception.class)
-    @ResponseBody
-    public ResponseEntity<Object> handleControllerException(HttpServletRequest req, Exception ex) {
-        return new ResponseEntity<>("111", HttpStatus.CONFLICT);
-    }*/
-
     @ExceptionHandler(value = MessagingException.class)
     public ResponseEntity<ErrorModelBean> handleVerifyEmail(Exception ex) {
-        ErrorModelBean errorModelBean = new ErrorModelBean("111" + "Not send email" + ex.getMessage(),
-                HttpStatus.BAD_REQUEST.value());
+        ErrorModelBean errorModelBean = new ErrorModelBean(DEFAULT_MESSAGE + "Not send email" + ex.getMessage());
 
         return new ResponseEntity<>(errorModelBean, HttpStatus.BAD_REQUEST);
     }
-
-    /*@Override
-    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
-        return null;
-    }*/
-
-    /*@Override
-    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        Map<String,String> responseBody = new HashMap<>();
-        responseBody.put("path",request.getContextPath());
-        responseBody.put("message","111" + "The URL you have reached is not in service at this time (404).");
-        return new ResponseEntity<>(responseBody, HttpStatus.NOT_FOUND);
-    }*/
-/*
-    @ExceptionHandler(value = RuntimeException.class)
-    public ResponseEntity<ErrorModelBean> handleError(RuntimeException ex) {
-
-        ErrorModelBean errorModelBean =new ErrorModelBean(ex.getMessage(),2);
-
-        return new ResponseEntity(errorModelBean, HttpStatus.INTERNAL_SERVER_ERROR);
-    }*/
 
 }
