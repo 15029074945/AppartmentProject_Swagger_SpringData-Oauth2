f0b2066c9f1cd082dd88503a65e627284706ce82
edgar <strazh.goroda@gmail.com>
2017-03-31 20:23:16 +0300

    fix UserController

--- src/main/java/com/spd/controller/UserController.java
+++ src/main/java/com/spd/controller/UserController.java
@@ -4,89 +4,70 @@ import com.spd.bean.ImageBean;
 import com.spd.bean.UserInformationBean;
 import com.spd.bean.UserRegistrationBean;
 import com.spd.entity.User;
-import com.spd.exception.AuthenticationUserException;
-import com.spd.exception.ValidationException;
 import com.spd.mapper.ObjectMapper;
+import com.spd.service.CheckService;
+import com.spd.service.RegistrationService;
 import com.spd.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import javax.mail.MessagingException;
-import java.security.Principal;
-import java.util.Optional;
-
-@Order(Ordered.HIGHEST_PRECEDENCE)
 @RestController
 @RequestMapping("api/v1/users")
 @Api(value = "users")
 public class UserController {
 
     private final UserService userService;
+    private final RegistrationService registrationService;
     private final ObjectMapper objectMapper;
+    private final CheckService checkService;
 
     @Autowired
-    public UserController(UserService userService, ObjectMapper objectMapper) {
+    public UserController(UserService userService, RegistrationService registrationService, ObjectMapper objectMapper, CheckService checkService) {
         this.userService = userService;
+        this.registrationService = registrationService;
         this.objectMapper = objectMapper;
+        this.checkService = checkService;
     }
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ApiOperation(value = "create update user", httpMethod = "POST")
     public ResponseEntity<String> createUser(Authentication authentication, @RequestBody UserRegistrationBean userRegistrationBean) {
-        Optional<Authentication> authenticationOptional = Optional.ofNullable(authentication);
-        if (authenticationOptional.isPresent()) {
-            throw new AuthenticationUserException("User is creates");
-        }
-        else {
-            ValidationException
-                    .assertTrue(userRegistrationBean.getTermsChecked(), "User shutdown exception handling");
-
-            User user = null;
-            try {
-                user = userService.createUser(userRegistrationBean);
-            } catch (Exception e) {
-                return new ResponseEntity<>("Error save. User is registered", HttpStatus.BAD_REQUEST);
-            }
-            try {
-                userService.registration(user);
-            } catch (MessagingException e) {
-                return new ResponseEntity<>("Error send message", HttpStatus.OK);
-            }
-            return new ResponseEntity<>("Message send", HttpStatus.OK);
-        }
+        checkService.checkAuthentication(authentication);
+        checkService.checkUserTerm(userRegistrationBean);
+        User user = userService.createUser(userRegistrationBean);
+        registrationService.registration(user);
+        return new ResponseEntity<>("Message send", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/pass/{password}", method = RequestMethod.POST)
+    @RequestMapping(value = "/pass", method = RequestMethod.POST)
     @ApiOperation(value = "change password", httpMethod = "POST")
-    public void changePassword(Authentication authentication, @PathVariable("password") String password) {
+    public void changePassword(Authentication authentication, @RequestBody String password) {
+        checkService.checkAuthentication(authentication);
         userService.changePassword(authentication.getName(), password);
     }
 
     @RequestMapping(value = "/verify/{token}", method = RequestMethod.POST)
     @ApiOperation(value = "verify user", httpMethod = "POST")
     public void verificationUser(@PathVariable("token") String token) {
-        userService.verification(token);
+        registrationService.verification(token);
     }
 
     @RequestMapping(value = "", method = RequestMethod.PUT)
     @ApiOperation(value = "create update user", httpMethod = "PUT")
     public void updateUser(Authentication authentication, @RequestBody UserInformationBean userInformationBean) {
-        userService
-                .updateUser(Optional
-                        .ofNullable(authentication)
-                        .map(Principal::getName), userInformationBean);
+        checkService.checkAuthentication(authentication);
+        userService.updateUser(authentication.getName(), userInformationBean);
     }
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     @ApiOperation(value = "get user", httpMethod = "GET")
     public UserInformationBean getUser(Authentication authentication) {
+        checkService.checkAuthentication(authentication);
         User user = userService.getByEmail(authentication.getName());
         return objectMapper.map(user, UserInformationBean.class);
     }
@@ -94,12 +75,14 @@ public class UserController {
     @RequestMapping(value = "", method = RequestMethod.DELETE)
     @ApiOperation(value = "delete user", httpMethod = "DELETE")
     public void deleteUser(Authentication authentication) {
+        checkService.checkAuthentication(authentication);
         userService.deleteUser(authentication.getName());
     }
 
     @RequestMapping(value = "/{id}/image", method = RequestMethod.PUT)
     @ApiOperation(value = "set image", httpMethod = "PUT")
     public void setUserImage(Authentication authentication , @RequestBody ImageBean imageBean) {
+        checkService.checkAuthentication(authentication);
         userService.setImage(authentication.getName(), imageBean.getId());
     }
 
