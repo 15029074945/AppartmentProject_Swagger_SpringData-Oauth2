1004198386d4c0e5f498701410becef6c640f36f
edgar <strazh.goroda@gmail.com>
2017-04-01 13:43:15 +0300

    Added host to properties

--- src/main/java/com/spd/service/EmailService.java
+++ src/main/java/com/spd/service/EmailService.java
@@ -2,6 +2,7 @@ package com.spd.service;
 
 import com.spd.exception.MessagingUserException;
 import com.spd.util.EmailUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
@@ -9,15 +10,18 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.Properties;
 
 @Service
 public class EmailService {
 
+    @Value("mail.front.host")
+    private String frontName;
+
+    private static final String VERIFY_EMAIL_TOKEN = "/verify-email?token=";
+    private static final String TO_CONFIRM_THE_EMAIL_CLICK_THE_LINK = "To confirm the email, click the link: ";
+
     private static final String SUBJECT = "Verify Booking";
-    private static final String VERIFY = "To confirm the email, click the link: ";
 
     public void sendMail(String to, String body) {
 
@@ -41,25 +45,6 @@ public class EmailService {
     }
 
     public String createLink(String token) {
-
-        String hostName = null;
-        InetAddress inetAddress = null;
-
-        try {
-            inetAddress = InetAddress.getLocalHost();
-            hostName = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(hostName);
-
-        // TODO: get domain with details
-        //String remoteAddress = ((WebAuthenticationDetails)(SecurityContextHolder.getContext().getAuthentication().getDetails()))
-        //        .getRemoteAddress();
-        //return VERIFY + "https://" + remoteAddress + "/verify-email?access_token=" + token;
-        // "https://house-hunter.herokuapp.com/verify-email?token="
-
-        return VERIFY + hostName + "/verify-email?token=" + token;
+        return TO_CONFIRM_THE_EMAIL_CLICK_THE_LINK + frontName + VERIFY_EMAIL_TOKEN + token;
     }
 }
