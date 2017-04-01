464f08ca9560a5e2e10fba0cdbe7b5b8e129bb75
edgar <strazh.goroda@gmail.com>
2017-04-01 13:11:14 +0300

    fix EmailService

--- src/main/java/com/spd/service/EmailService.java
+++ src/main/java/com/spd/service/EmailService.java
@@ -1,16 +1,16 @@
 package com.spd.service;
 
+import com.spd.exception.MessagingUserException;
 import com.spd.util.EmailUtil;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
-import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Properties;
 
 @Service
@@ -19,7 +19,7 @@ public class EmailService {
     private static final String SUBJECT = "Verify Booking";
     private static final String VERIFY = "To confirm the email, click the link: ";
 
-    public void sendMail(String to, String body) throws MessagingException {
+    public void sendMail(String to, String body) {
 
         Properties props = EmailUtil.getProperties();
         Session session = EmailUtil.getSession();
@@ -35,18 +35,31 @@ public class EmailService {
             Transport.send(message);
 
         } catch (Exception e) {
-            throw new MessagingException();
+            throw new MessagingUserException("No send email");
         }
 
     }
 
     public String createLink(String token) {
 
+        String hostName = null;
+        InetAddress inetAddress = null;
+
+        try {
+            inetAddress = InetAddress.getLocalHost();
+            hostName = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(hostName);
+
         // TODO: get domain with details
         //String remoteAddress = ((WebAuthenticationDetails)(SecurityContextHolder.getContext().getAuthentication().getDetails()))
         //        .getRemoteAddress();
         //return VERIFY + "https://" + remoteAddress + "/verify-email?access_token=" + token;
+        // "https://house-hunter.herokuapp.com/verify-email?token="
 
-        return VERIFY + "https://house-hunter.herokuapp.com/verify-email?token=" + token;
+        return VERIFY + hostName + "/verify-email?token=" + token;
     }
 }
