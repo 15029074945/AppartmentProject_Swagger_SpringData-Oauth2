40c25fefba111a9a133d2e1df2d2b2749e7ce41f
sanlan81 <sanlan81@gmail.com>
2017-04-03 15:35:32 +0300

    #5  modified  ImageService

--- src/main/java/com/spd/service/ImageService.java
+++ src/main/java/com/spd/service/ImageService.java
@@ -1,9 +1,16 @@
 package com.spd.service;
 
 import com.spd.entity.Image;
+import com.spd.exception.ImageException;
 import com.spd.repository.ImageRepository;
+import com.spd.validator.FileValidator;
+import net.sf.jmimemagic.Magic;
+import net.sf.jmimemagic.MagicMatch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class ImageService {
@@ -15,10 +22,24 @@ public class ImageService {
         this.imageRepository = imageRepository;
     }
 
-    public Image saveImage(String mimeType, byte[] data) {
+    public Image saveImage(MultipartFile imageFile)
+            throws IOException, ImageException
+    {
+        String mimeType;
+        FileValidator fileValidator = new FileValidator();
+
+        try {
+            MagicMatch match = Magic.getMagicMatch(imageFile.getBytes());
+            mimeType = match.getMimeType();
+        } catch (Exception e) {
+            mimeType = "";
+        }
+
+        fileValidator.validate(mimeType, imageFile);
+
         Image image = new Image();
         image.setMimeType(mimeType);
-        image.setData(data);
+        image.setData(imageFile.getBytes());
         imageRepository.save(image);
         return image;
     }
