9588bd2450ac71a29f7bb62020f12ebf510806ce
sanlan81 <sanlan81@gmail.com>
2017-04-03 15:38:25 +0300

    #26  modified  entity Conversation

--- src/main/java/com/spd/entity/Conversation.java
+++ src/main/java/com/spd/entity/Conversation.java
@@ -8,16 +8,15 @@ import java.util.List;
 @Table(name = "CONVERSATION")
 public class Conversation {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(name = "CREATED_DATE")
-    @Temporal(TemporalType.DATE)
     private Date createdDate;
 
     @Column(name = "UPDATED_DATE")
-    @Temporal(TemporalType.DATE)
     private Date updatedDate;
 
     @Column(name = "ACTIVE")
@@ -88,4 +87,7 @@ public class Conversation {
     public void setMessages(List<Message> messages) {
         this.messages = messages;
     }
+
+
+
 }
