4d7dec299d03bdba32bb729bba85afb8ddb7b076
sanlan81 <sanlan81@gmail.com>
2017-04-03 15:20:14 +0300

    #26  modified  ConversationBean

--- src/main/java/com/spd/bean/ConversationBean.java
+++ src/main/java/com/spd/bean/ConversationBean.java
@@ -1,5 +1,34 @@
 package com.spd.bean;
 
+import java.util.List;
+
 public class ConversationBean {
-    // TODO
+    private Integer id;
+    private List<Integer> attenders;
+    private Integer announcementId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public List<Integer> getAttenders() {
+        return attenders;
+    }
+
+    public void setAttenders(List<Integer> attenders) {
+        this.attenders = attenders;
+    }
+
+    public Integer getAnnouncementId() {
+        return announcementId;
+    }
+
+    public void setAnnouncementId(Integer announcementId) {
+        this.announcementId = announcementId;
+    }
 }
